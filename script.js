1623889b31c543d6d81148e62caf0a2c6f94a4c1
blxckroses <colyne.thomas300403@gmail.com>
2023-05-16 20:08:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let classes = [
+    "Paladin",
+    "Warriror",
+    "Dark Knight",
+    "Gunbreaker",
+    "White Mage",
+    "Scholar",
+    "Astrologian",
+    "Sage",
+    "Monk",
+    "Dragoon",
+    "Ninja",
+    "Samurai",
+    "Reaper",
+    "Bard",
+    "Machinist",
+    "Dancer",
+    "Black Mage",
+    "Summoner",
+    "Red Mage",
+    "Blue Mage"
+  ];
+
+  function random (list) {
+    //Array.isArray(list) vérifie que c'est list est bien un tableau
+    //console.log(Array.isArray(list)); //affiche dans la console
+    if (Array.isArray(list)) {
+
+        let resultat = list[Math.floor(Math.random() * list.length)]; 
+        return resultat;
+    }
+}
+console.log(random(4)); 
